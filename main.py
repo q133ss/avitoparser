71d02d2164a8a6051429f8fa53fccdd48db9830a
q133 <blockstar2k@gmail.com>
2022-01-10 01:48:16 +0300

    screen manager

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class ParserApp(App):
     def build(self):
-        #Виджеты
+        #Менеджер окон
+        self.sm = ScreenManager()
+        
+        #Первое окно
+        main_screen = Screen(name='main')
+        
+        #Виджеты окна
         bl = BoxLayout(orientation='vertical')
         self.query = TextInput(hint_text='Запрос')
         bl.add_widget(self.query)
@@ -18,7 +26,28 @@ class ParserApp(App):
         self.number = TextInput(hint_text='Кол-во объявлений')
         bl.add_widget(self.number)
         bl.add_widget(Button(text='Начать!', on_press=(self.parse)))
-        return bl
+        
+        #Добавляем виджеты к окну
+        main_screen.add_widget(bl)
+
+        #Второе окно
+        done_screen = Screen(name='done')
+
+        #Виджеты для второго окна
+        done_layout = BoxLayout(orientation='vertical')
+        done_layout.add_widget(Label(text='Объявления успешно спаршены! Они находяться в файле ads.csv'))
+        done_layout.add_widget(Button(text='Назад', on_press=(self.back)))
+        done_screen.add_widget(done_layout)
+
+        #Добавляем окна в менеджер
+        self.sm.add_widget(main_screen)
+        self.sm.add_widget(done_screen)
+
+        #Задаем изначальное окно
+        self.sm.current = 'main'
+
+        return self.sm
+
 
     def parse(self, instance):
 
@@ -65,7 +94,7 @@ class ParserApp(App):
                 data.append([link, price, description])
 
             #открываем файл на запись
-            with open('products.csv', 'w', encoding='utf-8') as ouf:
+            with open('ads.csv', 'w', encoding='utf-8') as ouf:
             #перебираем элементы списка d
                 for i in data:
                     #преобразуем элемент списка в строку
@@ -76,6 +105,15 @@ class ParserApp(App):
                     i=i.replace("]", "")
                     #записываем строку в файл
                     ouf.write(i + '\n')
+
+            #Меняем окно
+            self.sm.transition.direction = "left"
+            self.sm.current = "done"
+
+    #Метод, который возвращает нас назад
+    def back(self, instance):
+        self.sm.transition.direction = "right"
+        self.sm.current = 'main'
 
 
 
